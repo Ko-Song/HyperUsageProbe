aa61b49038cb8823cf0415ce3404b8e5488f8c9f
Aksd-M/S <131961097+Ko-Song@users.noreply.github.com>
2026-09-08 00:15:43 +0800

    更新 HookLog.java

--- app/src/main/java/io/github/kosong/hyperusageprobe/util/HookLog.java
+++ app/src/main/java/io/github/kosong/hyperusageprobe/util/HookLog.java
@@ -1,1 +1,23 @@
- 
+package io.github.kosong.hyperusageprobe.util;
+
+import de.robv.android.xposed.XposedBridge;
+
+public final class HookLog {
+
+    public static final String PREFIX = "[HyperUsageProbe] ";
+
+    private HookLog() {
+    }
+
+    public static void i(String message) {
+        XposedBridge.log(PREFIX + message);
+    }
+
+    public static void e(String message, Throwable throwable) {
+        XposedBridge.log(PREFIX + "ERROR: " + message);
+
+        if (throwable != null) {
+            XposedBridge.log(throwable);
+        }
+    }
+}
